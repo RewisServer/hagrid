c49899b3a70fee79d20ad499ac34119a8208bc23
Tobias B <germansuperioz@yahoo.de>
2020-12-01 12:16:24 +0100

    Add spigot module

---
+++ hagrid-kafka-spigot/src/main/java/dev/volix/rewinside/odyssey/hagrid/HagridSpigotPlugin.java
@@ -0,0 +1,15 @@
+package dev.volix.rewinside.odyssey.hagrid;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Tobias Büser
+ */
+public class HagridSpigotPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO connect to kafka and stuff
+    }
+
+}
